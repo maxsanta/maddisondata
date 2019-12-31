b8ba9b6613c1a372c61bfe80463ae244a94101b6
maxsanta <max.scl@gmail.com>
2019-12-30 22:02:23 -0300

    commit 1

--- maddison_animation.R
+++ maddison_animation.R
@@ -5,6 +5,7 @@ library(dplyr)
 library(ggplot2)
 library(scales)
 library(gganimate)
+library(extrafont)
 library(gdata)
 
 # load data
@@ -13,6 +14,7 @@ View(mpd2018) #está en formato tibble
 str(mpd2018)
 head(mpd2018)
 View(df)
+
 # covert date to Date class
 # mpd2018$year <- as.Date(mpd2018$year, format="%Y%M%D", origin="1")
 
@@ -23,7 +25,7 @@ df <- subset(mpd2018,
 
 a<-"GDP per capita (1800-2010)"
 ggplot(df, aes(x = year, y = cgdppc, color = country)) +
-  geom_line(size = 0.8) +
+  geom_line(size = 0.5) +
     theme_bw() +
   xlim(1950,2017) +
   labs(x = NULL, y = "GDP per capita (1990 Int. GK dollars)\n", color = NULL,
@@ -31,21 +33,24 @@ ggplot(df, aes(x = year, y = cgdppc, color = country)) +
 
 ?'%>%'
 
-myfuncion<-function(a, b){
+myfuncion<-function(a, b,c=30000 v){
   df <- subset(mpd2018, 
-               year>=a & 
-                 countrycode %in% c("ARG", "CHL", "KOR" , "PHL"))
-  ggplot(df, aes(x = year, y = cgdppc, color = country)) +
+               year>=a & year<=b & 
+                 countrycode %in% v)
+  
+  ggplot(df, aes(x = year, y = rgdpnapc, color = country)) +
     geom_line(size = 0.8) +
     theme_bw() +
-    xlim(a,b) +
-    labs(x = NULL, y = "GDP per capita (1990 Int. GK dollars)\n", color = NULL,
+    scale_y_continuous(limits = c(0, c)) +
+    labs(x = NULL, y = "GDP per capita (1990 Int. GK dollars)\n", color = NULL, 
          title = "GDP per capita")
   
 }
-a<-1950
-b<-1980
-paises<-c("ARG", "CHL", "KOR" , "PHL")
-myfuncion(a,b)
-paises  
+a<-1970
+b<-2017
+
+paises<-c("ARG", "CHL", "KOR", "IRL", "MAL")
+v<-paises
+myfuncion(a,b, paises)
+
 
